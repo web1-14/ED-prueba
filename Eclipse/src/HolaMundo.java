6eeee6c2fb2830423640711ba7e8a0baf8e5c954
web1-14 <79098560+web1-14@users.noreply.github.com>
2021-03-14 20:50:01 +0100

    Update HolaMundo.java

--- Eclipse/src/HolaMundo.java
+++ Eclipse/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Holita vecinito");
+		System.out.println("Gracias puerta");
 	}
 
 }
